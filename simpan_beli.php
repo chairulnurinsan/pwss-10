99296076dd3214112ec80c18887a9d068469b7cc
cnichairul <chairulnurinsan@gmail.com>
2022-12-05 16:45:22 +0700

    lokal ke github

---
+++ simpan_beli.php
@@ -0,0 +1,34 @@
+<?php
+$namacust = $_POST['namacust'];
+$email = $_POST['email'];
+$notelp = $_POST['notelp'];
+$tanggal = date ("Y-m-d");
+$barang_pilih = '';
+$qty = 1;
+
+$dataValid = "YA";
+if (strlen (trim($namacust))==0) { 
+    echo "Nama Harus Diisi.. <br/>"; 
+    $dataValid="TIDAK";
+}
+if (strlen (trim($notelp))==0) {
+    echo "No. Telp Harus Diisi.. <br/>"; 
+    $dataValid="TIDAK";
+}
+if (isset($_COOKIE['keranjang'])) {
+    $barang_pilih = $_COOKIE ['keranjang'];
+}else{
+    echo "Keranjang Belanja Kosong <br/>"; 
+    $dataValid="TIDAK";
+}
+if ($dataValid == "TIDAK") {
+    echo "Masih Ada Kesalahan, silakan perbaiki! </br>"; 
+    echo "<input type='button' value='Kembali'
+            onClick='self.history.back()'>";
+    exit;
+}
+
+echo "Data siap disimpan.";
+
+setcookie ('keranjang', $barang_pilih,time()-3600);
+?>
